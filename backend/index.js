b48996648b4ff87766fa1e1c3f40ab7066738912
jeongdh <jeongdh1212@gmail.com>
2023-05-09 00:52:41 +0900

    chat GPT API 활용 웹 서비스

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-let apiKey = ""; // api 생성한 키값
+// api 생성한 키값
+// let apiKey = "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";
+let apiKey = "";
 
 const { Configuration, OpenAIApi } = require("openai"); //openai 설치
 const serverless = require("serverless-http");
