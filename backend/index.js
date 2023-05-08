64e7a5c181fff41cd12cf1c8069e54e83c6eafdc
jeongdh <jeongdh1212@gmail.com>
2023-05-08 17:48:14 +0900

    GPT API 활용한 웹서비스

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+let apiKey = ""; // api 생성한 키값
+
+const { Configuration, OpenAIApi } = require("openai"); //openai 설치
+const serverless = require("serverless-http");
+const express = require("express"); // express 설치
+const cors = require("cors"); // cors 설치
+const configuration = new Configuration({
+  apiKey: apiKey,
+});
+const openai = new OpenAIApi(configuration);
+const app = express();
+
+// CORS 이슈 해결
+let corsOptions = {
+  origin: "https://dateguide.pages.dev",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //12~13줄: body-parser 기능을 위해 선언
+
+app.post("/guide", async function (req, res) {
+  let { myPlace, myConcept, userMessages, assistantMessages } = req.body;
+  let message = [
+    {
+      role: "user",
+      content: `${myPlace} ${myConcept} 데이트하려고 합니다. 컨셉에 맞는 데이트 코스를 추천해주세요.`,
+    },
+  ];
+
+  while (userMessages.length != 0 || assistantMessages.length != 0) {
+    if (userMessages.length != 0) {
+      message.push({
+        role: "user",
+        content: String(userMessages.shift().replace(/\n/g, "<br />")),
+      });
+    }
+    if (assistantMessages.length != 0) {
+      message.push({
+        role: "assistant",
+        content: String(assistantMessages.shift().replace(/\n/g, "<br />")),
+      });
+    }
+  }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0.8,
+    top_p: 0.8,
+    max_tokens: 1000, //최대 2048
+    frequency_penalty: 0.6,
+    presence_penalty: 0.3, // 숫자 낮을수록 반복적인 대답 X
+    messages: message,
+  });
+  let guide = completion.data.choices[0].message["content"];
+  console.log(guide);
+  // res.send(guide);
+  res.json({ assistant: guide }); // 결과값을 json으로 전송한다
+});
+
+// app.listen(3000);
+module.exports.handler = serverless(app);
---
+++ frontend/script.js
@@ -0,0 +1,78 @@
+const btnSend = document.querySelector("#btnSendMessage");
+const chatMessage = document.querySelector(".chat_message");
+const place = document.querySelector("#place");
+const concept = document.querySelector("#concept");
+const chatCon = document.querySelector(".chat_con");
+const guideChat = document.querySelector(".guide_chat");
+const loder = document.querySelector(".loder");
+const restart = document.querySelector(".restart");
+const chatInputDiv = document.querySelector(".chat-input");
+
+chatInputDiv.style.display = "none";
+chatCon.style.display = "none";
+loder.style.display = "none";
+restart.style.display = "none";
+place.focus();
+
+let userMessages = [];
+let assistantMessages = [];
+
+const sendMessage = async () => {
+  guideChat.style.display = "none";
+  loder.style.display = "block";
+  chatInputDiv.style.display = "flex";
+
+  // value값 서버에 보내기(json 형태로 전송)
+  let myPlace = place.value;
+  let myConcept = concept.value;
+
+  // chatting
+  const chatInput = document.querySelector(".chat-input input");
+  const chatMessageDiv = document.createElement("div");
+  chatMessageDiv.classList.add("chat_message");
+  chatMessageDiv.innerHTML = `<p>${chatInput.value}</p>`;
+  chatCon.appendChild(chatMessageDiv);
+
+  userMessages.push(chatInput.value);
+  chatInput.value = "";
+
+  //
+  const response = await fetch(
+    "https://wkrhlz7zzk.execute-api.ap-northeast-2.amazonaws.com/props/guide",
+    // "http://localhost:3000/guide",
+    {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        myPlace: myPlace,
+        myConcept: myConcept,
+        userMessages: userMessages,
+        assistantMessages: assistantMessages,
+      }),
+    }
+  );
+
+  const data = await response.json();
+  assistantMessages.push(data.assistant);
+  const astrologerMessage = document.createElement("div");
+  astrologerMessage.classList.add("chat_message");
+  astrologerMessage.innerHTML = `<p class="assistant">${data.assistant.replace(
+    /\n/g,
+    "<br />"
+  )}</p>`;
+  chatCon.appendChild(astrologerMessage);
+
+  chatCon.style.display = "block";
+  chatCon.scrollTop = chatCon.scrollHeight;
+  loder.style.display = "none";
+  restart.style.display = "block";
+};
+
+const reStart = () => {
+  window.location.reload();
+};
+btnSend.addEventListener("click", sendMessage);
+document.querySelector("#btn").addEventListener("click", sendMessage);
+restart.addEventListener("click", reStart);
